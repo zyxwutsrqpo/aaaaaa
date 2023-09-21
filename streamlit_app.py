b2b6ccdf8159c64b7d075220a6301f15ad72985b
zyxwutsrqpo <144980917+zyxwutsrqpo@users.noreply.github.com>
2023-09-21 04:31:56 +0000

    h

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-import streamit as at
+import streamlit as st
 import random
 st.title("おみくじアプリ")
-if st.buttun("おみくじを引く")
-results=["大吉"、"中吉"、"小吉"、"吉"、"凶"、"大凶"]
-result=random.choice(results)
-st.write(f"結果:{result}")
+if st.buttun("おみくじを引く"):
+    results = ["大吉"、"中吉"、"小吉"、"吉"、"凶"、"大凶"]
+    result = random.choice(results)
+    st.write(f"結果:{result}")
